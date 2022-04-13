d0ed264d201a77037a10df8f381f570774644d44
Devansh Mathur <drdevansh2003@gmail.com>
2022-04-13 10:32:17 +0530

    TTS UPDATED

--- tts.py
+++ tts.py
@@ -1,9 +1,11 @@
 from gtts import gTTS 
 from playsound import playsound
+import os
 
 def speak(obj):
-    t1 = gTTS(text=str(obj), lang='en-in', slow=False)  
+    t1 = gTTS(text=str(obj), lang='en', slow=False)  
     t1.save("welcome.mp3")
     playsound("welcome.mp3")
+    os.remove("welcome.mp3")
 
 #speak(input())
